3fd3dcfdc4af213fc1702afb8e290aa22829e9d2
JoeWroe <joe_wroe@icloud.com>
2016-04-28 09:23:38 +0100

    Add #current_position
    ∆ Add unit test, state and behaviour to have a current position. In
    order to have a position that can change, it needs a current position
    to begin with. Currently the MarsRover class also includes the
    behaviour to return this position as a string since this is required
    later. Not comfortable with this class managing a presentation string,
    but until a presentation class is constructed, this will have to
    remain here.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,12 +1,31 @@
 class MarsRover
 
+  attr_reader :current_position
+
   def initialize(compass:)
     @compass = compass
+    @current_position = current_position
   end
 
   def possible_compass_directions
     compass.headings
   end
+
+  def current_position
+    []
+  end
+
+  def print_current_position
+    current_position.join(", ")
+  end
+
+  # def set_heading(heading)
+  #   compass.headings.each do |headings|
+  #     if heading == headings
+  #       heading << current_position
+  #     end
+  #   end
+  # end
 
   private
 
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -4,6 +4,8 @@ describe MarsRover do
 
   subject(:mars_rover) { described_class.new(compass: compass) }
 
+  let(:start_position) { 'N' }
+
   let(:compass) { double(:compass, headings: possible_directions) }
   let(:possible_directions) { ['N', 'E', 'S', 'W']}
 
@@ -11,6 +13,10 @@ describe MarsRover do
 
     it 'can understand compass directions' do
       expect(mars_rover.possible_compass_directions).to eq(possible_directions)
+    end
+
+    it 'can have a current position' do
+      expect(mars_rover.print_current_position).to eq(start_position)
     end
   end
 end
